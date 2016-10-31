70f5a787bad4e3faab21c62124892ccbd376e212
amateusz <grzywomat@gmail.com>
2016-10-31 23:30:56 +0100

    added setup file.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='my-orange-client',
+    version='0.4',
+    packages=[''],
+    url='https://github.com/amateusz/my-orange-client',
+    license='ffa',
+    author='amateusz',
+    author_email='grzywomat@gmail.com',
+    description='A little tool to login to "My Orange" mobile operator account and check how much internet you have left for example',
+    install_requires=[
+        requests_oauthlib,
+        requests,
+        bs4,
+        json,
+        urllib.parse,  # python3 specific
+        getpass
+    ]
+)
