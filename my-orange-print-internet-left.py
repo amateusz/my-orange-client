b508cf4f17285d6796b138899ace4197bcd8faf1
Mateusz Grzywacz <amateusz.grzywacz@gmail.com>
2019-03-13 12:37:14 +0100

    fixed this small script (even more concise!)

--- my-orange-print-internet-left.py
+++ my-orange-print-internet-left.py
@@ -1,24 +1,21 @@
 # -*- coding: utf-8 -*-
-import my_orange_client as orange
+from my_orange_client import MyOrangeClient as Orange
 
 try:
     # try to authorize. if fails, then ask for new credientals
-    t = orange.handleToken()
+    orange = Orange()
+    t = orange.giveMeToken()
 except IOError:
     print('Brak pliku z tokenem. Zaloguj się')
     import getpass
 
     u = input('Login: ')
     try:
-        t = orange.handleToken(u, getpass.getpass('Hasło :'))
+        t = orange.giveMeToken(u, getpass.getpass('Hasło :'))
         print(t)
     except PermissionError:
         # now it means that credentials are wrong. exit
         print('Złe dane logowania. Zamykam')
         exit(-1)
-contract = orange.getContractData(t)
-if contract[0] is True:
-    s = orange.getInfoServices(t, contract[1]['msisdn'])[1]
-    print(s['MBamount'])
 else:
-    print('ERR: ' + contract[1])
+    print(orange.getGBamount())
